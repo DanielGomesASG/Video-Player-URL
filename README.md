fbbd0658ddfd3dd41ad686efc768c8b3103954ed
Daniel Gomes <danielgomesasg@gmail.com>
2023-11-10 16:06:12 -0300

    Vídeo Player URL

---
+++ README.md
@@ -0,0 +1,3 @@
+# Video Player URL
+
+Player de vídeo feito em JavaScript em que você pode fornecer uma URL para receber o conteúdo de media dela, e até mesmo baixá-lo clicando nos 3 pontos localizados no canto inferior direito do player. O arquivo "TESTVIDEOS.json" contém diversos vídeos para teste.
---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var buttonLoad = document.getElementById("buttonLoad");
+  var videoURL = document.getElementById("videoUrl");
+  var videoPlayer = document.getElementById("videoPlayer");
+
+  buttonLoad.addEventListener("click", function () {
+    loadVideo(videoURL.value);
+  });
+
+  function loadVideo(url) {
+    // Limpar mensagens de erro anteriores
+    videoURL.setCustomValidity("");
+
+    // Validar a URL do vídeo
+    if (isValidVideoUrl(url)) {
+      videoPlayer.src = url;
+      videoPlayer.load();
+    } else {
+      // Exibir mensagem de erro se a URL for inválida
+      videoURL.setCustomValidity("URL do vídeo inválida.");
+      videoURL.reportValidity();
+    }
+  }
+
+  function isValidVideoUrl(url) {
+    // Validação simples
+    return /^(http|https):\/\/[^ "]+$/.test(url);
+  }
+});
